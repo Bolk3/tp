c69f02416d76ffcab281eaf23e59b6ec3f7290f3
Bolk3 <63613351+Bolk3@users.noreply.github.com>
2022-01-14 16:40:26 +0100

    initialisation tp7

---
+++ Tp7.py
@@ -0,0 +1,27 @@
+def Existe(f:str, int:list):
+    """[summary]
+
+    Args:
+        f (str): [description]
+        int (list): [description]
+    """
+    pass
+
+def New_int(f:str, int:list):
+    """[summary]
+
+    Args:
+        f (str): [description]
+        int (list): [description]
+    """
+    pass
+
+def Dichotomie(f:str, int:list, crit_x):
+    """[summary]
+
+    Args:
+        f (str): [description]
+        int (list): [description]
+        crit_x ([type]): [description]
+    """
+    pass
