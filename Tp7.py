a78e9a4dc6a0164ed079dc05e8eae5ce89225e1b
Bolk3 <63613351+Bolk3@users.noreply.github.com>
2022-01-18 14:58:15 +0100

    session 07/01/22 14h57

--- Tp7.py
+++ Tp7.py
@@ -1,24 +1,3 @@
-def Range_f(debut, arrive, pas=1):
-    """fonction range valable avec les float 
-
-    Args:
-        debut (int ou float): debut de l'intervale
-        arrive (int ou float): fin de l'intervale
-        pas (int, optional): pas entre les valeur. par defaut à 1.
-
-    Returns:
-        r (list): resultat
-    """
-    r = [debut]
-    i = debut
-    while i < arrive:
-        i += pas
-        if len(str(pas)) != i:
-            pass
-        r.append(i)
-    return r
-
-
 def Existe(f:str, int:list):
     """verifie si dans un intervale donnée la fonction f a au moins une racine
 
@@ -29,46 +8,14 @@ def Existe(f:str, int:list):
     Returns:
         r (bool): True si une racine est trouvée dans la fonction
     """
-    list_eval = []
-    #creation de toute les possibles variation de f
-    #determination du pas nessesaire
-    g,d = str(int[0]).split("."), str(int[1]).split(".")
-    if len(g) == 1 and len(d) == 1:
-        pas = 1
-    else:
-        if len(g) > len(d):
-            pas = 1/10**len(g[1])
-        elif len(g) < len(d):
-            pas = 1/10**len(d[1])
-        else:
-            if len(g[len(g)-1]) > len(d[len(d)-1]):
-                pas = 1/10**len(g[len(g)-1])
-            else:
-                pas = 1/10**len(d[len(d)-1])
-    
-    if int[0] > int[1]:
-        for i in Range_f(int[1], int[0], pas):
-            temp = f.replace("x", str(i))
-            list_eval.append(temp)
-    else:
-        for i in Range_f(int[0], int[1],pas):
-            temp = f.replace("x", str(i))
-            list_eval.append(temp)
-    
-    #evaluation de toutes les possibilités
-    negpos = False
-    if eval(list_eval[0]) < 0:
-        for poss in list_eval:
-            if eval(poss) >= 0:
-                negpos = True
-    else:
-        for poss in list_eval:
-            if eval(poss) <= 0:
-                negpos = True
-    
-    if negpos == True:
+    list_eval = [eval(f.replace("x", str(int[0])))
+                 , eval(f.replace("x", str(int[1])))]
+    if list_eval[0] <= 0 and list_eval[1] >= 0:
         return True
-    return f"la fonction f(x) ne contient pas de racine dans l'intervale {int}"
+    elif list_eval[0] >= 0 and list_eval[1] <= 0:
+        return True
+    else:
+        return False
 
 def New_int(f:str, int:list):
     """
@@ -77,7 +24,7 @@ def New_int(f:str, int:list):
         f (str): fonction dans laquelle on recherche une racine 
         int (list): l'intervalle dans lequel la racine est recherchée 
     """
-    moyenne = int[0]+int[1]/2
+    moyenne = (int[0]+int[1])/2
     g_int, d_int = [int[0], moyenne], [moyenne, int[1]]
     if g_int[0] > g_int[1]:
         d_gauche = g_int[0] - g_int[1]
